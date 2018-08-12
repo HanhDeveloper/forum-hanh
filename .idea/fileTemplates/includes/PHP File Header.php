9bedf2c0648e4a23369f8247498edb5f417b5636
HanhDeveloper <hanh.cho.do@gmail.com>
2018-08-12 21:49:39 +0700

    php file header

--- .idea/fileTemplates/includes/PHP File Header.php
+++ .idea/fileTemplates/includes/PHP File Header.php
@@ -1,3 +1,6 @@
 /**
- * @author: Hanh <hanh.cho.do@gmail.com>
+ * @package      HDev
+ * @author       Hanh <hanh.cho.do@gmail.com>
+ * @copyright    2018 by Hanh Developer
+ * @link         https://fb.com/hnv.97
  */
